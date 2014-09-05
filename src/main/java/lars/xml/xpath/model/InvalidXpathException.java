3179a161c89af5a84610fa84205b9c4e591a021d
Lars Schneider <derlarsschneider@github.com>
2014-09-05 11:40:34 +0200

    Eclipse warnings removed

--- src/main/java/lars/xml/xpath/model/InvalidXpathException.java
+++ src/main/java/lars/xml/xpath/model/InvalidXpathException.java
@@ -2,4 +2,9 @@ package lars.xml.xpath.model;
 
 public class InvalidXpathException extends Exception {
 
+	/**
+	 * Generated
+	 */
+	private static final long serialVersionUID = -7260583052996719245L;
+
 }
